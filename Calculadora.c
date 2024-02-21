c3c822f95366cc71b57dcb90d2e57f95ad7272fd
Belceb45 <rubiodiego001@gmail.com>
2024-02-21 17:38:21 -0600

    Primer codigo

---
+++ Calculadora.c
@@ -0,0 +1,61 @@
+// Realizar una calcualdora y subirla a git
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+// Prototipos
+void suma(float *, float *,float *);
+//  void resta(float *,float *,float *);
+//  void multiplicacion(float *,float *,float*);
+//  void division(float *,float *,float*);
+
+int main()
+{
+    char opt[30];
+    float a, b,res;
+    int condicion = 0;
+
+    printf("\nCalculadora\nA: ");
+    scanf("%f", &a);
+    printf("B: ");
+    scanf("%f", &b);
+
+    printf("\n\nIngrese la opcion: ");
+    scanf("%s", opt);
+    do
+    {
+        if (stricmp(opt, "suma") == 0)
+        {
+            printf("\nSuma");
+            suma(&a,&b,&res);
+            printf("\nResultado: %.1f",res);
+        }
+        else if (stricmp(opt, "resta") == 0)
+        {
+            printf("\nResta");
+        }
+
+        else if (stricmp(opt, "multiplicacion") == 0)
+        {
+            printf("\nmMultiplicacion");
+        }
+        else if (stricmp(opt, "division") == 0)
+        {
+            printf("\nDivision");
+        }
+        else
+        {
+            printf("\nIntente de nuevo");
+            return 0;
+        }
+
+    } while (condicion);
+
+}
+
+
+void suma(float *x, float *y,float *res)
+{
+    *res = (*x) + (*y);
+}
