5060356eec60457582702187d61e8568df2741e6
Belceb_45 <70941083+Belceb45@users.noreply.github.com>
2024-02-21 18:12:53 -0600

    Add files via upload
    Base de código

---
+++ Calculadora.c
@@ -0,0 +1,51 @@
+// Realizar una calcualdora y subirla a git
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+
+int main()
+{
+    char opt[30];
+    float a, b,res;
+    int condicion = 0;
+
+    printf("\nCalculadora\nA: ");
+    scanf("%f", &a);
+    printf("B: ");
+    scanf("%f", &b);
+
+    printf("\n\nIngrese la opcion: ");
+    scanf("%s", opt);
+    do
+    {
+        if (stricmp(opt, "suma") == 0)
+        {
+            printf("\nSuma");
+          
+        }
+        else if (stricmp(opt, "resta") == 0)
+        {
+            printf("\nResta");
+        }
+
+        else if (stricmp(opt, "multiplicacion") == 0)
+        {
+            printf("\nmMultiplicacion");
+        }
+        else if (stricmp(opt, "division") == 0)
+        {
+            printf("\nDivision");
+        }
+        else
+        {
+            printf("\nIntente de nuevo");
+            return 0;
+        }
+
+    } while (condicion);
+
+}
+
+
